c495e89cf82b99bd17e11ef2c2c94587c4c2acd6
Meitar M <meitarm@gmail.com>
2020-02-13 15:53:12 -0500

    Make sure Step 3 can verify correctly.

--- scaling-ssh-authentication-with-certificates/step3-verify.sh
+++ scaling-ssh-authentication-with-certificates/step3-verify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-grep -q "^@cert-authority host01,localhost,127.0.0.1" ~/.ssh/known_hosts \
+grep -q "^@cert-authority host01,localhost,127.0.0.1" /root/.ssh/known_hosts \
     && echo done # Validation successful.
